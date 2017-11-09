fb2ef8858f922a118f5e9558396e94073525739f
htayhlaingkyaw <htayhlaingkyaw92@gmail.com>
2017-11-10 01:24:04 +0630

    POC screen Implementation

---
+++ app/src/main/java/com/padcmyanmar/poc_screen_implementation/POCScreenImplementationApp.java
@@ -0,0 +1,17 @@
+package com.padcmyanmar.poc_screen_implementation;
+
+import android.app.Application;
+
+/**
+ * Created by HHK on 11/10/2017.
+ */
+
+public class POCScreenImplementationApp extends Application {
+
+    public static final String LOG_TAG = "POCScreenImplementationApp";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ app/src/main/java/com/padcmyanmar/poc_screen_implementation/viewholders/CinemaFragmentViewHolder.java
@@ -0,0 +1,14 @@
+package com.padcmyanmar.poc_screen_implementation.viewholders;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by HHK on 11/10/2017.
+ */
+
+public class CinemaFragmentViewHolder extends RecyclerView.ViewHolder {
+    public CinemaFragmentViewHolder(View itemView) {
+        super(itemView);
+    }
+}
