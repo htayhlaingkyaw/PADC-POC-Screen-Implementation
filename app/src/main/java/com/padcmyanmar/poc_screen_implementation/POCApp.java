e537aa9952834b812d440eda35197a05e1530da5
htayhlaingkyaw <htayhlaingkyaw92@gmail.com>
2017-11-10 01:32:23 +0630

    modified

--- app/src/main/java/com/padcmyanmar/poc_screen_implementation/POCApp.java
+++ app/src/main/java/com/padcmyanmar/poc_screen_implementation/POCApp.java
@@ -6,9 +6,9 @@ import android.app.Application;
  * Created by HHK on 11/10/2017.
  */
 
-public class POCScreenImplementationApp extends Application {
+public class POCApp extends Application {
 
-    public static final String LOG_TAG = "POCScreenImplementationApp";
+    public static final String LOG_TAG = "POCApp";
 
     @Override
     public void onCreate() {
