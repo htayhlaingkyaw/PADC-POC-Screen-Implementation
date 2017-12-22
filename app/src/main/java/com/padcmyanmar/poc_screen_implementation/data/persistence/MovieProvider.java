adb90c65becc8ad3ce0ed1f9ae9942644e48b3bc
DESKTOP-SPPJS1R\Aspire <htayhlaingkyaw92@gmaill.com>
2017-12-22 12:51:16 +0630

    mod

---
+++ app/src/main/java/com/padcmyanmar/poc_screen_implementation/data/persistence/MovieProvider.java
@@ -0,0 +1,57 @@
+package com.padcmyanmar.poc_screen_implementation.data.persistence;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Aspire on 12/22/2017.
+ */
+
+public class MovieProvider extends ContentProvider {
+
+    private static final UriMatcher sUriMatcher = buildUriMatcher();
+    private MovieDBHelper mDBHelper;
+
+    private static UriMatcher buildUriMatcher() {
+        UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+        return uriMatcher;
+    }
+
+    @Override
+    public boolean onCreate() {
+        return false;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(@NonNull Uri uri, @Nullable String[] strings, @Nullable String s, @Nullable String[] strings1, @Nullable String s1) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues contentValues) {
+        return null;
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String s, @Nullable String[] strings) {
+        return 0;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues contentValues, @Nullable String s, @Nullable String[] strings) {
+        return 0;
+    }
+}
